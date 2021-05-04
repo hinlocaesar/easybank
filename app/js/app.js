305ab91088fb431c31c4a524f0d332cf6b29e12d
Caesar <caesar.hinlo.tech@gmail.com>
2021-05-04 16:16:16 +0800

    Added source code for mobile header

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+
+const btnHambuger = document.querySelector('#btnHamburger');
+
+btnHambuger.addEventListener('click',function(){
+    console.log("[LOG HINLO] click hinlo hamburger");
+    if(btnHambuger.classList.contains('open')){
+        btnHambuger.classList.remove('open');
+    }else{
+        btnHambuger.classList.add('open');
+    }
+
+})
