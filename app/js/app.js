7faa784fba6477445f1d9eb99a662ed6fe750b32
Caesar <caesar.hinlo.tech@gmail.com>
2021-05-10 14:13:48 +0800

    Added Transistion for overlay

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,19 @@
 
 const btnHambuger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHambuger.addEventListener('click',function(){
     console.log("[LOG HINLO] click hinlo hamburger");
     
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ //close
         header.classList.remove('open');
-    }else{
-
-        header.classList.add('open');    
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');    
+    }else{ //open
+        header.classList.add('open');   
+        overlay.classList.remove('fade-out'); 
+        overlay.classList.add('fade-in');    
     }
 
 })
