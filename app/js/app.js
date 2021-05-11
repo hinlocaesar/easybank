b9a053110107ff6727e54fcb861ce70149cc3f7a
Caesar <caesar.hinlo.tech@gmail.com>
2021-05-11 13:02:02 +0800

    Finalize modification for mobile overlay and animations

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,30 @@
-
 const btnHambuger = document.querySelector('#btnHamburger');
+const body = document.queryCommandEnabled('.body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade')
 
 btnHambuger.addEventListener('click',function(){
     console.log("[LOG HINLO] click hinlo hamburger");
     
     if(header.classList.contains('open')){ //close
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');    
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');    
+        })
+
+
+
     }else{ //open
         header.classList.add('open');   
-        overlay.classList.remove('fade-out'); 
-        overlay.classList.add('fade-in');    
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out'); 
+            element.classList.add('fade-in'); 
+        })
+
+
+         
     }
 
 })
